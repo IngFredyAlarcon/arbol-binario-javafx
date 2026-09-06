855951158138d3e0ae4d7cbf38338d14b9ef93e9
FELPBEC <juan.becerra20@uptc.edu.co>
2026-09-06 14:15:11 -0500

    Juan Felipe Becerra Salamanca METODOS DE GUARDAR Se añadieron los métodos de guardar en memoria tanto para el TreeManager como para el MainViewController

---
+++ binarytree/src/main/java/co/edu/uptc/domain/model/BinaryTree.java
@@ -0,0 +1,32 @@
+package co.edu.uptc.domain.model;
+
+public class BinaryTree {
+    private Node root;
+    private String name;
+    
+    public BinaryTree() {
+    }
+
+    public BinaryTree(String name) {
+        this.name = name;
+        root = null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+    
+
+}
