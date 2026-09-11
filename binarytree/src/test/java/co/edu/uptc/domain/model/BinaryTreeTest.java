4772d6ceb60ca50c3adfd5f0cedc4e62d0b78254
Andres Niño <andresalejandroninoaraujo@gmail.com>
2026-09-10 23:02:23 -0500

    correcion, antes para las pruebas unitarias usaba el metodo set.root mas ya no es necesario

--- binarytree/src/test/java/co/edu/uptc/domain/model/BinaryTreeTest.java
+++ binarytree/src/test/java/co/edu/uptc/domain/model/BinaryTreeTest.java
@@ -11,18 +11,16 @@ class BinaryTreeTest {
     @Test
     @DisplayName("Verificar recorridos del árbol (PreOrder, InOrder, PostOrder, LevelOrder)")
     void pruebaRecorrido() {
-    Node root = new Node(10);
-    root.setLeft(new Node(5));
-    root.setRight(new Node(15));
-    root.getLeft().setLeft(new Node(2));
-    root.getLeft().setRight(new Node(7));
+        BinaryTree tree = new BinaryTree("Árbol Test");
+        tree.insert(10);
+        tree.insert(5);
+        tree.insert(15);
+        tree.insert(2);
+        tree.insert(7);
 
-    BinaryTree tree = new BinaryTree("Árbol Test");
-    tree.setRoot(root);
-
-    assertEquals(List.of(10, 5, 2, 7, 15), tree.getPreOrder());
-    assertEquals(List.of(2, 5, 7, 10, 15), tree.getInOrder());
-    assertEquals(List.of(2, 7, 5, 15, 10), tree.getPostOrder());
-    assertEquals(List.of(10, 5, 15, 2, 7), tree.getLevelOrder());
-}
+        assertEquals(List.of(10, 5, 2, 7, 15), tree.getPreOrder());
+        assertEquals(List.of(2, 5, 7, 10, 15), tree.getInOrder());
+        assertEquals(List.of(2, 7, 5, 15, 10), tree.getPostOrder());
+        assertEquals(List.of(10, 5, 15, 2, 7), tree.getLevelOrder());
+    }
 }
